5eba995cfa3d3e6dac78278d993ddc024ed3a9d2
Rushil Choksi <55545647+rushilchoksi@users.noreply.github.com>
2023-06-30 18:26:47 +0530

    Added browser window parameters

--- configure-fleet.py
+++ configure-fleet.py
@@ -14,6 +14,8 @@ IP_ADDR = str(sys.argv[1])
 PASS = str(sys.argv[2])
 
 options = Options()
+options.add_argument('--window-size=1920,1080')
+options.add_argument('--start-maximized')
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
